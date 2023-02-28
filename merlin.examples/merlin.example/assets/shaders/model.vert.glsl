a0162ecfa8bc4805dc7db518b4d743844418cf08
Jules Topart <jules.topart@gmail.com>
2023-02-28 15:24:00 +0100

    Squashed commit of the following:
    commit 2fe18da86d3854f50c966360c1ffebfd560aadb5
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Tue Feb 28 15:19:29 2023 +0100
    
        update example
    
    commit 611ae9189312e8d2516079cdd1f0329e41d7c369
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Mon Feb 27 17:17:34 2023 +0100
    
        Implement Abstract Texture class
    
    commit ff8da124b604d253cb1f89b2cf8cea4f94dd0322
    Author: JulesTopart <jules.topart@gmail.com>
    Date:   Fri Feb 24 10:00:30 2023 +0100
    
        isosurface algortithm
    
    commit 32a309da7af17de14f11a7bd00fb33aea07c5826
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Sun Feb 19 13:10:00 2023 +0100
    
        Isosurface
    
    commit f5aa9dd8b5394c95ede794a35a924153257fcd05
    Merge: c005f10 458cf62
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Thu Feb 2 22:30:58 2023 +0100
    
        Merge branch 'main' into dev
    
    commit c005f10c3d4f4954e2ed6f9b27d89f5c362983cd
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Mon Jan 30 20:46:48 2023 +0100
    
        Implement Spatial Hasing with SPH Heat transfer
    
    commit be5e12bc25498a0c28a987c9137c2ce5fa26075b
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Mon Jan 30 00:31:56 2023 +0100
    
        Refactor Memory Buffers
    
    commit 3e801d48d551f287022e78f6a2431586ec5477e0
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Wed Jan 25 08:36:57 2023 +0100
    
        WIP : Adding neighbor search routine
    
    commit d766ba24d5b5370400ecd233cba7b18e43a94230
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Tue Jan 24 07:12:15 2023 +0100
    
        Implementing spatial hashing
    
    commit 58887161b5a61976eca850e311499fd687ca11d4
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Mon Jan 23 19:58:01 2023 +0100
    
        dev(Heat) : Implement heat sph 3D
    
    commit 3b8f30ed7306c34a7ff5588bd410a71e9b89e723
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Mon Jan 23 00:22:38 2023 +0100
    
        dev(Heat) : Implement Laplacian heat transfer
    
    commit e2a28578c4ceac027bf0d513ad8840176f6ba884
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Sun Jan 22 10:26:21 2023 +0100
    
        dev(Heat) : WIP Heat transfer experiment
    
    commit 8ed5ee97dc391852915788fe7c133bd853856477
    Author: Jules Topart <jules.topart@gmail.com>
    Date:   Sat Jan 21 11:43:52 2023 +0100
    
        feat(Experiments) : Add Experiments on dev branch

--- merlin.examples/merlin.example/assets/shaders/model.vert.glsl
+++ merlin.examples/merlin.example/assets/shaders/model.vert.glsl
@@ -3,10 +3,12 @@
 layout (location = 0) in vec3 _position;
 layout (location = 1) in vec3 _normal;
 layout (location = 2) in vec3 _color;
+layout (location = 3) in vec2 _texCoord;
 
 out vec3 position;
 out vec3 normal;
 out vec3 color;
+out vec2 texCoord;
 
 uniform vec3 lightPos;
 uniform vec3 lightColor;
@@ -20,6 +22,7 @@ void main() {
 	position = vec3(model * vec4(_position, 1.0f));
 	color = _color;
 	normal = _normal;
+	texCoord = _texCoord;
 
 	gl_Position = view * vec4(position, 1.0f);
 }
