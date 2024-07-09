f234e1cb810f6c7f5518449a8fd8b07397cc0c9b
Jules Topart <jules.topart@gmail.com>
2024-07-09 16:01:42 +0200

    pelton

---
+++ merlin.projects/merlin.water.pelton/src/AppLayer.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "settings.h"
+
+using namespace Merlin;
+
+#define UNUSED 0
+#define SOLID 1
+#define FLUID 2
+#define FLUID_EMITTER 3
+#define GRANULAR 4
+#define BOUNDARY 5
+
+class AppLayer : public Merlin::Layer3D {
+public:
+	AppLayer(){}
+
+	void onAttach() override;
+	void onDetach() override;
+	void onEvent(Merlin::Event& event) override;
+	void onUpdate(Merlin::Timestep ts) override;
+	void onImGuiRender() override;
+
+	void InitGraphics();
+	void InitPhysics();
+	void ResetSimulation();
+	void SpawnParticle();
+
+	void SyncUniforms();
+
+	void NeigborSearch();
+	void Simulate(Merlin::Timestep ts);
+
+private:
+
+	// --- Graphics ---
+
+	Scene scene;
+	Renderer renderer;
+	
+	StagedComputeShader_Ptr solver;
+	ComputeShader_Ptr isoGen;
+	StagedComputeShader_Ptr prefixSum;
+
+	ParticleSystem_Ptr ps;
+	ParticleSystem_Ptr bs;
+
+	Mesh_Ptr static_emitter;
+	Mesh_Ptr pelton;
+
+	/*
+	IsoSurface_Ptr isosurface;
+	Texture3D_Ptr volume;
+	Texture2D_Ptr texture_debug;
+	*/
+
+	Shader_Ptr particleShader;
+	Shader_Ptr binShader;
+
+	// --- Simulation--- 
+	Settings settings;
+	GLuint numParticles = 0;
+	GLuint numEmitter = 0;
+
+	glm::vec3 model_matrix_translation = { 0.0f, 0.0f, 0.0f };
+
+	double nns_time = 0;
+	double jacobi_time = 0;
+	double solver_substep_time = 0;
+	double solver_total_time = 0;
+	double render_time = 0; double render_start_time = 0;
+	double total_time = 0; double total_start_time = 0;
+
+	float elapsedTime = 0;
+	float lastSpawTime = 0;
+	bool paused = true;
+	bool integrate = true; 
+	bool use_isosurface = false;
+
+	float rot_speed = 2;
+	float sim_speed = 1;
+	float camera_speed = 1;
+	bool mousePressed = false;
+
+};
